0c8cf2e9258e2c4a58084b56b3e6d1610af18d84
LX050724 <yaojizhong@outlook.com>
2021-01-29 21:59:10 +0800

    单片机代码移至RM_LIB，添加spdUtil命名空间

---
+++ modules/loggerFactory/include/spdUtil.h
@@ -0,0 +1,17 @@
+/**
+ * @file spdUtil.h
+ * @author yao
+ * @date 2021年1月21日
+ */
+
+#ifndef KDROBOTCPPLIBS_SPDUTIL_H
+#define KDROBOTCPPLIBS_SPDUTIL_H
+
+#include <string>
+
+namespace spdUtil {
+    std::string formatSize(size_t size);
+}
+
+
+#endif //KDROBOTCPPLIBS_SPDUTIL_H
