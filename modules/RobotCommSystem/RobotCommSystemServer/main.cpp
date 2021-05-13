e5d85b07a46c2680f54f7a83038e0d8078a0dc47
LX050724 <yaojizhong@outlook.com>
2021-05-13 19:30:22 +0800

    RobotCommSystemServer 改用MainThread

--- modules/RobotCommSystem/RobotCommSystemServer/main.cpp
+++ modules/RobotCommSystem/RobotCommSystemServer/main.cpp
@@ -6,62 +6,11 @@
  */
 
 #include <QCoreApplication>
-#include <spdlog/spdlog.h>
-#include <RCS_Server.h>
-#include <spdlogger.h>
+#include "main.h"
 
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
-
-    QCommandLineParser parser;
-    QCommandLineOption noUdp("noUDP", "Disable Udp radio");
-    QCommandLineOption log({"l", "log"}, "Set the log file path. default disable", "log");
-    QCommandLineOption TcpPort({"t", "TcpPort"}, "Set Tcp Port, the default is 8550", "TcpPort");
-    TcpPort.setDefaultValue("8550");
-
-    parser.addHelpOption();
-    parser.addOptions({noUdp, log, TcpPort});
-    parser.process(a);
-
-    QString logFile = parser.value("log");
-    if (!logFile.isEmpty()) {
-        spdlogger::allLogger_logToFile(logFile.toStdString());
-    }
-    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [thread %t] [%^%-8l%$]: %v");
-    spdlogger logger(__FUNCTION__);
-    logger.info("Build Time: {} {}", __DATE__, __TIME__);
-
-#if defined(__DEBUG__)
-    spdlog::set_level(spdlog::level::debug);
-    logger.info("Build type: Debug");
-#else
-    logger.info("Build type: Release");
-#endif
-
-    uint16_t port = 8550;
-    if (parser.isSet(noUdp))
-        logger.info("no Udp mod");
-
-    if (parser.isSet(TcpPort)) {
-        QString portString = parser.value(TcpPort);
-        bool Ok;
-        uint16_t t = portString.toUShort(&Ok);
-        if (Ok) {
-            port = t;
-            logger.info("set Tcp port {}", port);
-        } else {
-            logger.error("Tcp port input error");
-            return 0;
-        }
-    }
-
-    RCS_Server *server;
-    try {
-        server = new RCS_Server(port, !parser.isSet(noUdp));
-        (void) server;
-    } catch (const std::runtime_error &e) {
-        logger.error(e.what());
-        return -1;
-    }
-    return a.exec();
+    QCoreApplication app(argc, argv);
+    MyMainThread myMainThread(app.arguments());
+    QObject::connect(&myMainThread, SIGNAL(threadExit()), &app, SLOT(quit()));
+    return app.exec();
 }
